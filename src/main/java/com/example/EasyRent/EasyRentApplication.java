4a2e191d8194ac69816705c8497ef039987085f1
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-02-08 12:28:29 -0500

    Jose_Armando_LT_Creacion_Del_encarpetado

---
+++ src/main/java/com/example/EasyRent/EasyRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.EasyRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EasyRent/EasyRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EasyRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
